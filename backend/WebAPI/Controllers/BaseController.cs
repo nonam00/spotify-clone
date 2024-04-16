f52b2d83dfce50774753a15cf4948ad709d3cf5b
nonam00 <raisky.ilya3@yandex.ru>
2024-04-16 19:25:55 +0300

    Added abstract base controller Added empty songs controller

---
+++ backend/WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
+
+        internal Guid UserId => !User.Identity!.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+    }
+}
---
+++ backend/WebAPI/Controllers/SongsController.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiVersionNeutral]
+    [Produces("application/json")]
+    [Route("{version:apiVersion}/songs")]
+    public class SongsController(IMapper mapper) : BaseController
+    {
+        private readonly IMapper _mapper = mapper;  
+    }
+}
