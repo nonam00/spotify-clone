b5568cef3bb7b67b74370444ad06ca7deaee51b7
nonam00 <raisky.ilya3@yandex.ru>
2024-04-18 21:06:22 +0300

    Added command for song uploading

--- backend/Application/Songs/Commands/CreateSong/CreateSongCommand.cs
+++ backend/Application/Songs/Commands/CreateSong/CreateSongCommand.cs
@@ -1,7 +1,13 @@
-﻿namespace Application.Songs.Commands.CreateSong
-{
-    public class CreateSongCommand
-    {
+﻿using MediatR;
 
+namespace Application.Songs.Commands.CreateSong
+{
+    public class CreateSongCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string SongPath { get; set; }
+        public string ImagePath { get; set; }
     }
 }
---
+++ backend/Application/Songs/Commands/CreateSong/CreateSongCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Domain;
+using MediatR;
+
+namespace Application.Songs.Commands.CreateSong
+{
+    public class CreateSongCommandHandler(ISongsDbContext dbContext)
+        : IRequestHandler<CreateSongCommand, Guid>
+    {
+        private readonly ISongsDbContext _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateSongCommand request,
+            CancellationToken cancellationToken)
+        {
+            var song = new Song
+            {
+                Id = Guid.NewGuid(),
+                UserId = request.UserId,
+                Title = request.Title,
+                Author = request.Author,
+                CreatedAt = DateTime.UtcNow,
+                SongPath = request.SongPath,
+                ImagePath = request.ImagePath
+            };
+
+            await _dbContext.Songs.AddAsync(song, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return song.Id;
+        }
+    }
+}
