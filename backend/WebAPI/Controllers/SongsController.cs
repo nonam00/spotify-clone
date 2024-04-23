390409b6fc844c80e896a501908ce4ba52ee688f
nonam00 <raisky.ilya3@yandex.ru>
2024-04-23 20:55:40 +0300

    Added POST method for uploading new song

--- backend/WebAPI/Controllers/SongsController.cs
+++ backend/WebAPI/Controllers/SongsController.cs
@@ -6,6 +6,8 @@ using Application.Songs.Commands.CreateSong;
 using Application.Songs.Queries.GetSongList;
 using Application.Songs.Queries.GetSongList.GetSongListByTitle;
 
+using WebAPI.Models;
+
 namespace WebAPI.Controllers
 {
     [ApiVersionNeutral]
@@ -16,14 +18,14 @@ namespace WebAPI.Controllers
         private readonly IMapper _mapper = mapper;
 
         /// <summary>
-        /// Gets search results
+        /// Gets songs satisfying the search request
         /// </summary>
         /// <remarks>
         /// GET /search/hysteria
         /// </remarks>
         /// <param name="searchString">User search query by song title</param>
+        /// <returns>Returns SongListVm</returns>
         /// <respose code="200">Success</respose>
-        /// <returns></returns>
         [HttpGet("search/{searchString}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<SongListVm>> GetSongListByTitle(string searchString)
@@ -35,6 +37,31 @@ namespace WebAPI.Controllers
 
             var vm = await Mediator.Send(query);
             return Ok(vm);
+        }
+        /// <summary>
+        /// Creates new song 
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// POST /post
+        /// {
+        ///     
+        /// }
+        /// </remarks>
+        /// <param name="createSongDto">createSongDto object</param>
+        /// <returns>Returns id (guid)</returns>
+        /// <respose code="200">Success</respose>
+        /// <response code="401">If the user is unathorized</response>
+        //[Authorize]
+        [HttpPost("post")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<Guid>> AddNewSong([FromBody] CreateSongDto createSongDto)
+        {
+            var command = _mapper.Map<CreateSongCommand>(createSongDto);
+            command.UserId = UserId;
+            var songId = await Mediator.Send(command);
+            return Ok(songId);
         }
     }
 }
