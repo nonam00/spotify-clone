0d2ff66ab15a59ff64cc97b221c13aa27249bfa2
nonam00 <raisky.ilya3@yandex.ru>
2024-04-23 20:03:00 +0300

    Added data transfer objects for API

---
+++ backend/WebAPI/Models/CreateSongDto.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using System.ComponentModel.DataAnnotations;
+
+using Application.Common.Mappings;
+using Application.Songs.Commands.CreateSong;
+
+namespace WebAPI.Models
+{
+    public class CreateSongDto : IMapWith<CreateSongCommand>
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string SongPath { get; set; }
+        [Required]
+        public string ImagePath { get; set; }
+        
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateSongDto, CreateSongCommand>()
+                .ForMember(command => command.Title,
+                    opt => opt.MapFrom(dto => dto.Title))
+                .ForMember(command => command.Author,
+                    opt => opt.MapFrom(dto => dto.Author))
+                .ForMember(command => command.SongPath,
+                    opt => opt.MapFrom(dto => dto.SongPath))
+                .ForMember(command => command.ImagePath,
+                    opt => opt.MapFrom(dto => dto.ImagePath));
+        }
+    }
+
+}
