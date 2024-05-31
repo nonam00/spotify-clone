784302f350a5c0f47599f4a9039c57f0870c35e1
nonam00 <raisky.ilya3@yandex.ru>
2024-05-31 23:12:09 +0300

    Added method for getting song by id

---
+++ backend/Application/Songs/Queries/GetSongById/GetSongByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Songs.Queries.GetSongById
+{
+    public class GetSongByIdQuery : IRequest<SongVm>
+    {
+        public Guid SongId { get; set; }
+    }
+}
