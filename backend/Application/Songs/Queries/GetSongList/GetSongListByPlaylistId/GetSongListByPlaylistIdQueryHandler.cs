7a0c850a308ce0c5a2339342aa199771c4a6fd46
nonam00 <raisky.ilya3@yandex.ru>
2024-06-19 23:15:12 +0000

    Added method for getting all songs from playlist

---
+++ backend/Application/Songs/Queries/GetSongList/GetSongListByPlaylistId/GetSongListByPlaylistIdQueryHandler.cs
@@ -0,0 +1,38 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+using Application.Interfaces;
+
+namespace Application.Songs.Queries.GetSongList.GetSongListByPlaylistId
+{
+    public class GetSongListByPlaylistIdQueryHandler
+      : IRequestHandler<GetSongListByPlaylistIdQuery, SongListVm>
+    {
+        private readonly ISongsDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetSongListByPlaylistIdQueryHandler(
+            ISongsDbContext dbContext,
+            IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<SongListVm> Handle(
+            GetSongListByPlaylistIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var songs = await _dbContext.PlaylistSongs
+              .Where(ps => ps.PlaylistId == request.PlaylistId)
+              .OrderBy(ps => ps.CreatedAt)
+              .Select(ps => ps.Song)
+              .ProjectTo<SongVm>(_mapper.ConfigurationProvider)
+              .ToListAsync(cancellationToken);
+            
+            return new SongListVm { Songs = songs };
+        }
+    }
+}
