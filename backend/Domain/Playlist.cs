d2144346c9221f318b8d1c1ec3d5728e516ba5f0
nonam00 <raisky.ilya3@yandex.ru>
2024-06-20 01:53:10 +0000

    Added data base migration Minor fixes

--- backend/Domain/Playlist.cs
+++ backend/Domain/Playlist.cs
@@ -6,7 +6,7 @@ namespace Domain
         public Guid UserId { get; set; }
         public string Title { get; set; } = null!;
         public string? Description { get; set; }
-        public string? ImagePath { get; set; };
+        public string? ImagePath { get; set; }
         public DateTime CreatedAt { get; set; }
         public List<Song> Songs { get; set; } = new List<Song>();
     }
