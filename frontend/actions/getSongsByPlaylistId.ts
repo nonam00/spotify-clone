3189749f7115a33b874e6fe0cc9fd7f6fd326aae
nonam00 <raisky.ilya3@yandex.ru>
2024-06-21 23:40:06 +0000

    Added server action for getting playlist songs

--- frontend/actions/getSongsByPlaylistId.ts
+++ frontend/actions/getSongsByPlaylistId.ts
@@ -5,9 +5,9 @@ import { cookies } from "next/headers";
 import $api from "@/api/http";
 import { Song } from "@/types/types";
 
-const getLikedSongs = async (): Promise<Song[]> => {
+const getSongsByPlaylistId = async(playlistId: string): Promise<Song[]> => {
   try {
-    const { data } = await $api.get("/liked/get/", {
+    const { data } = await $api.get(`playlists/${playlistId}/songs`, {
       headers: {
         // needs to input manualy because of server actions 
         Cookie: cookies().getAll().map((cookie) => (
@@ -15,14 +15,15 @@ const getLikedSongs = async (): Promise<Song[]> => {
         ))
       }
     });
-    if(!(data?.likedSongs)) {
+    
+    if(!data?.songs) {
       return [];
     }
-    return data?.likedSongs as Song[];
-  } catch (error) {
-    console.log(error);
+    return data.songs as Song[];
+  } catch(error: any) {
+    console.log(error.message);
     return [];
   }
 }
 
-export default getLikedSongs;
+export default getSongsByPlaylistId;
