7843c4ec9586c702d7d259e20f5bb25d4da53fee
nonam00 <raisky.ilya3@yandex.ru>
2024-06-22 03:57:27 +0000

    Added playlist page

---
+++ frontend/app/playlist/components/PlaylistContent.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { Song } from "@/types/types"; 
+
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+
+import { useUser } from "@/hooks/useUser";
+import useOnPlay from "@/hooks/useOnPlay";
+
+interface PlaylistContentProps {
+  songs: Song[];
+}
+
+const PlaylistContent: React.FC<PlaylistContentProps> = ({
+  songs
+}) => {
+  const router = useRouter();
+  const { isLoading, isAuth } = useUser();
+
+  const onPlay = useOnPlay(songs);
+  
+  useEffect(() => {
+    if (!isLoading && !isAuth) {
+      console.log("replace");
+      router.replace('/');
+    }
+  }, [isLoading, isAuth, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div className="
+        flex
+        flex-col
+        gap-y-2
+        p-6
+        text-neutral-400
+        items-center
+        md:items-start
+      ">
+        There are no songs in this playlist.
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div
+          key={song.id}
+          className="flex items-center gap-4 w-full"
+        >
+          <div className="flex-1">
+            <MediaItem
+              onClick={(id: string) => onPlay(id)}
+              data={song}
+            />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
+export default PlaylistContent;
---
+++ frontend/app/playlist/page.tsx
@@ -0,0 +1,94 @@
+import getPlaylistById from "@/actions/getPlaylistById";
+import getSongsByPlaylistId from "@/actions/getSongsByPlaylistId";
+
+import Header from "@/components/Header"
+import PlaylistContent from "./components/PlaylistContent";
+import PlaylistImage from "@/components/PlaylistImage";
+
+export const revalidate = 0;
+
+interface SearchProps {
+  searchParams: {
+    id: string
+  }
+}
+
+const Playlist = async ({searchParams}: SearchProps) => {
+  const playlist = await getPlaylistById(searchParams.id);
+  if (!playlist) {
+    throw new Error("No playlist has been found!");
+  }
+  const songs = await getSongsByPlaylistId(playlist.id);
+  
+  return (
+    <div
+      className="
+        bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-y-auto
+      "
+    >
+      <Header>
+        <div className="mt-20">
+          <div
+            className="
+              flex
+              flex-col
+              md:flex-row
+              items-center
+              gap-x-5
+            "
+          >
+            <div className="
+              relative
+              h-32
+              w-32
+              lg:h-44
+              lg:w-44
+            ">
+              <PlaylistImage
+                playlist={playlist}
+              />
+            </div>
+            <div className="
+              flex
+              flex-col
+              gap-y-5
+              mt-4
+              md:mt-0
+            ">
+              <h1
+                className="
+                  text-white
+                  text-4xl
+                  sm:text-5xl
+                  lg:text-7xl
+                  font-bold
+                "
+              >
+                {playlist.title}
+              </h1>
+              <p className="
+                flex
+                md:box
+                font-semibold
+                md:text-sm
+                justify-center
+                md:justify-normal
+                text-neutral-300
+                md:text-white
+              ">
+                {playlist.description ?? "No description"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Header>
+      <PlaylistContent songs={songs}/>
+    </div>
+  );
+}
+
+export default Playlist;
